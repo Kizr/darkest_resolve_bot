076712ea68d46a0a086bbc3ca68f4ef8808a1969
Kizr <babraham002@gmail.com>
2017-12-05 11:22:55 +0000

    version 2

--- darkest_resolve_bot.py
+++ darkest_resolve_bot.py
@@ -12,13 +12,21 @@ def login_reddit():
 				user_agent = config.user_agent) 
 	return run
 				
-def run_darkest_bot(run, status, comment_list):
+def run_darkest_bot(run, status, comment_list, gbot):
+	bot = "darkest_resolve_bot"
 	print("Grabbing 10 comments")
-	for comment in run.subreddit('DarkestDungeon').comments(limit=30):
+	for comment in run.subreddit('Test').comments(limit=30):
 		if  "resolve is tested..." in comment.body.lower() and comment.id not in comment_list:
 			print("Matching comment found!")
-			comment.reply("#" + random.choice(status) + "\n\n ^^I ^^am ^^a ^^bot, ^^and ^^this ^^action ^^was ^^performed ^^automatically. ^^Please ^^contact ^^/u/Frozen_Aurora ^^if ^^you ^^have ^^any ^^questions ^^or ^^concerns.")
+			sname, stext = random.choice(list(status.items()))
+			comment.reply("#" + sname + "\n\n >" + stext + "\n\n &nbsp; \n\n ^^My ^^Master ^^is ^^/u/Frozen_Aurora.")
 			print("Writing reply and updating comment_list")
+			comment_list.append(comment.id)
+			
+			with open("comment_list.txt", "a") as f:
+				f.write(comment.id + "\n")
+		elif "good bot" in comment.body.lower() and comment.id not in comment_list and comment.parent().author == bot:
+			comment.reply(random.choice(gbot))
 			comment_list.append(comment.id)
 			
 			with open("comment_list.txt", "a") as f:
@@ -29,11 +37,21 @@ def run_darkest_bot(run, status, comment_list):
 	time.sleep(30)
 			
 def status_list():
-	status = ['Paranoid', 'Selfish', 'Irrational', 
-			  'Fearful', 'Hopeless', 'Abusive', 
-			  'Masochistic', 'Rapturous', 'Powerful',
-			  'Courageous', 'Stalwart', 'Vigorous',
-			  'Focused']
+	status = {
+			  'Paranoid':'"The walls close in, the shadows whisper of conspiracy."',
+			  'Selfish':'"Self-preservation is paramount, at any cost."',
+			  'Irrational':'"Reeling! Gasping! Taken over the edge, into madness."',
+			  'Fearful':'"Fear and frailty finally claim their due..."',
+			  'Hopeless':'"There can be no hope in this hell... no hope at all."',
+			  'Abusive':'"Frustration, and fury! ...More destructive than a hundred cannons."',
+			  'Masochistic':'"Those who covet injury, find it in no short supply."',
+			  'Powerful':'"Anger is power - unleash it!"',
+			  'Courageous':'"A moment of valor shines brightest against a backdrop of despair."',
+			  'Stalwart':'"Many fall in the face of chaos, but not this one!... not today."',
+			  'Vigorous':'"Adversities can foster hope, and resilience."',
+			  'Focused':'"A moment of clarity in the eye of the storm..."'
+			 }
+			 
 	return status
 	
 def replies_list():
@@ -46,13 +64,19 @@ def replies_list():
 			comment_list = list(filter(None, comment_list))
 	
 	return comment_list
+	
+def gbot_list():
+	gbot = ['Amusing.','Petty mortal, I do not want your praise.', 'Ancestor be praised!','*Gears whirr menacingly, it seems... pleased?*','Praise me whilst you can fleshling.']
+	
+	return gbot
 
 
 run = login_reddit()
 status = status_list()	
 comment_list = replies_list()
+gbot = gbot_list()
 
 
 while True:
-	run_darkest_bot(run, status, comment_list)
+	run_darkest_bot(run, status, comment_list, gbot)
 
