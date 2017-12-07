f8ce95c7290cd98a1a856fa16a84f23d4e3c8ae5
Kizr <babraham002@gmail.com>
2017-12-07 10:36:44 +0000

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-username = 'darkest_resolve_bot'
-password = 'Luffynami78' 
-client_id = 'aXiBOeDfyf0O1g' 
-client_secret = 'fKbLgHYpHBNI1qs16bzoOtrNobs'
-user_agent = "Darkest Dungeon Resolve Bot v2.0"
