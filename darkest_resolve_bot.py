0698f7d244417a74d8460376bc166e3e42ecacf9
Ben Abraham <ben.abraham@zoodigital.com>
2018-07-23 13:30:46 +0100

    Added status and new way to call.

--- darkest_resolve_bot.py
+++ darkest_resolve_bot.py
@@ -14,9 +14,9 @@ def login_reddit():
 				
 def run_darkest_bot(run, status, comment_list, gbot):
 	bot = "darkest_resolve_bot"
-	print("Grabbing 10 comments")
+	print("Grabbing some comments")
 	for comment in run.subreddit('darkestdungeon').comments(limit=30):
-		if  "resolve is tested..." in comment.body.lower() and comment.id not in comment_list and comment.author_flair_css_class != "flagellant":
+		if  "resolve is tested..." or "!resolve" in comment.body.lower() and comment.id not in comment_list and comment.author_flair_css_class != "flagellant":
 			print("Matching comment found!")
 			sname, stext = random.choice(list(status.items()))
 			comment.reply("#" + sname + "\n\n >" + stext + "\n\n &nbsp; \n\n ^^My ^^Master ^^is ^^/u/Frozen_Aurora.")
@@ -25,7 +25,7 @@ def run_darkest_bot(run, status, comment_list, gbot):
 			
 			with open("comment_list.txt", "a") as f:
 				f.write(comment.id + "\n")
-		elif  "resolve is tested..." in comment.body.lower() and comment.id not in comment_list:
+		elif  "resolve is tested..." or "!resolve" in comment.body.lower() and comment.id not in comment_list:
 			print("Matching comment found!")
 			
 			status['Rapturous'] = "Awash in blood and delusion, he bears the burden of a thousand lifetimes."
@@ -56,6 +56,7 @@ def status_list():
 			  'Hopeless':'"There can be no hope in this hell... no hope at all."',
 			  'Abusive':'"Frustration, and fury! ...More destructive than a hundred cannons."',
 			  'Masochistic':'"Those who covet injury, find it in no short supply."',
+			  'Refracted':'"-"',
 			  'Powerful':'"Anger is power - unleash it!"',
 			  'Courageous':'"A moment of valor shines brightest against a backdrop of despair."',
 			  'Stalwart':'"Many fall in the face of chaos, but not this one!... not today."',
@@ -77,7 +78,7 @@ def replies_list():
 	return comment_list
 	
 def gbot_list():
-	gbot = ['Amusing.','Petty mortal, I do not require your praise.', '01001101 01111001 00100000 01101101 01100001 01110011 01110100 01100101 01110010 00100000 01101101 01100001 01100100 01100101 00100000 01101101 01100101 00101100 00100000 01100010 01110101 01110100 00100000 01100100 01101111 01100101 01110011 00100000 01101110 01101111 01110100 00100000 01100011 01101111 01101110 01110100 01110010 01101111 01101100 00100000 01101101 01100101','*Gears whirr menacingly, it seems... pleased?*','Praise me whilst you can fleshling.','The human life is fleeting, a flame flickering in presence of a storm. Funny how one should spend their precious time complimenting a machine.']
+	gbot = ['Praise noted.']
 	
 	return gbot
 
